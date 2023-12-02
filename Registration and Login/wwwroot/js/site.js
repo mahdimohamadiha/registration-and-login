aeb7a1fc36d8236415b642245ac6d379f88c5f97
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2023-12-02 17:00:27 +0330

    Completing the 'Create Account'

--- Registration and Login/wwwroot/js/site.js
+++ Registration and Login/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//const togglePassword = document.querySelector('#togglePassword');
+//const password = document.querySelector('#id_password');
 
-// Write your JavaScript code.
+//togglePassword.addEventListener('click', function (e) {
+
+//    if (password.getAttribute('type') === 'password') {
+//        password.setAttribute('type', 'text');
+//        this.src = "/icons/eye-show.png"
+//    }
+//    else {
+//        password.setAttribute('type', 'password');
+//        this.src = "/icons/eye-hide.png"
+//    }
+//});
+
+function myFunction(pass_id, toggle_id) {
+    const togglePassword = document.querySelector(toggle_id);
+    const password = document.querySelector(pass_id);
+
+    togglePassword.addEventListener('click', function (e) {
+
+        if (password.getAttribute('type') === 'password') {
+            password.setAttribute('type', 'text');
+            this.src = "/icons/eye-show.png"
+        }
+        else {
+            password.setAttribute('type', 'password');
+            this.src = "/icons/eye-hide.png"
+        }
+    });
+}
+
+myFunction('#id_password' ,'#togglePassword')
+myFunction('#id_confirm_password', '#toggleConfirmPassword')
