5d0d747698eba5795813444932a488d2b5aa6f5e
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2023-12-21 01:53:01 +0330

    Completing the 'Sign In' UI

--- Registration and Login/Controllers/SignInController.cs
+++ Registration and Login/Controllers/SignInController.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Registration_and_Login.Data;
+using Registration_and_Login.Models;
 
 namespace Registration_and_Login.Controllers
 {
 	public class SignInController : Controller
 	{
+		private readonly ApplicationDbContext _db;
+
+		public SignInController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
 		public IActionResult Index()
 		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(string email, string password)
+		{
+			Users user = _db.Users.SingleOrDefault(u => u.Email == email);
+			if (user != null)
+			{
+				if (user.Password == password)
+				{
+					Console.WriteLine("True");
+					return View();
+				}
+			}
 			return View();
 		}
 	}
