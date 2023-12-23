86c5ff960c38ef498de0694de1dc6b4868bb0c18
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2023-12-24 01:46:59 +0330

    Perform part of 'Forgot Password' UI

--- Registration and Login/Controllers/SignInController.cs
+++ Registration and Login/Controllers/SignInController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OtpNet;
 using Registration_and_Login.Data;
 using Registration_and_Login.Models;
 
@@ -28,6 +29,28 @@ namespace Registration_and_Login.Controllers
 					return RedirectToAction("Index", "Welcome");
 				}
 			}
+			return View();
+		}
+
+		public IActionResult ForgotPassword()
+		{
+			// Generate a random secret key (you would typically store this securely on your server)
+			var secretKey = KeyGeneration.GenerateRandomKey(20);
+
+			// Create a Time-based OTP generator with a 1-minute timestep
+			var totp = new Totp(secretKey, step: 60);
+
+			// Get the current OTP
+			string code = totp.ComputeTotp();
+
+			// Print the generated code
+			Console.WriteLine($"Generated Code: {code}");
+
+			// Wait for one minute (for demonstration purposes)
+			Console.WriteLine("Waiting for 1 minute...");
+			System.Threading.Thread.Sleep(60000);
+
+			
 			return View();
 		}
 	}
